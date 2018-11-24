807b1541d1dd41d8b08eb4b4e2bd9c32d61de1fb
Unknown <yusnanda.hj@gmail.com>
2018-11-24 19:07:01 +0100

    Server handler, téléchargement fichier

--- src/main/java/server/FileController.java
+++ src/main/java/server/FileController.java
@@ -2,72 +2,107 @@ package server;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import helper.FileUploadResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import representation.File;
 import helper.FileUploadResponse;
 import representation.FileList;
+import service.FileStorageService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class FileController {
 
-    @GetMapping("/test")
-    public File test() throws IOException {
-        //Example
-        return new File("001","test.json");
-    }
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    private List<File> fileList = new ArrayList<>();
+
+    @Autowired
+    private FileStorageService fileStorageService;
 
     /**
-     * Rest GET Handler pour récupérer un unique fichier
+     * Rest GET Handler pour récupérer les données d'un unique fichier
+     *
      * @param fileId
      * @return
      */
     @GetMapping("/files/{fileId}")
-    public File getFile(@PathVariable String fileId){
-        //Example
-        return new File("0001","fichier1",1666666);
+    public ResponseEntity<Resource> getFile(@PathVariable String fileId, HttpServletRequest request) {
+        // Récupére les fichiers en tant que Resource
+        String fileName = null;
+        for(File file : fileList){
+            if(file.getFileid().equals(fileId)){
+                fileName = file.getName();
+            }
+        }
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        // Détermine le type de fichier
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Impossible de déterminer le type du fichier.");
+        }
+        //Type de fichier par défaut
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
     /**
      * Rest GET Handler pour récupérer une liste de fichiers
+     *
      * @param
      * @return
      */
     @GetMapping("/files")
-    public List<File> getFiles() {
-        //Example
-        List<File> fileList = new ArrayList<>();
-        fileList.add(new File("0001","fichier1",12));
-        fileList.add(new File("0002","fichier2",98));
-
-        return fileList;
+    public List<File> getFileList() {
+        /*Initialiser fileList en ce basant sur listeFichier.json*/
+        return this.fileList;
     }
+
 
     /**
      * Rest POST Handler pour envoyer un unique fichier
-     * @param fileId
+     *
+     * @param file
      * @return
      */
     @PostMapping("/files/{fileId}")
-    public FileUploadResponse uploadFile(@PathVariable String fileId) {
-        //Example
-        FileUploadResponse response = new FileUploadResponse();
-        response.setCle1(fileId);
-        response.setCle2("Toto");
-        response.setCle3("nintendo switch");
+    public File uploadFile(@RequestParam("file") MultipartFile file) {
+        String fileName = fileStorageService.storeFile(file);
 
-        return response;
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        File fileToAdd = new File("3P2P" + ts.getTime(), fileName, file.getSize());
+
+        fileList.add(fileToAdd);
+
+        return fileToAdd;
     }
 
     /**
      * Rest POST Handler pour envoyer une liste de fichiers
+     *
      * @param
      * @return
      */
@@ -86,6 +121,7 @@ public class FileController {
 
     /**
      * Rest DELETE Handler pour supprimer une liste de fichiers
+     *
      * @param
      * @return
      */
