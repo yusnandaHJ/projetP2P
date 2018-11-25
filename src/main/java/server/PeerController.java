29f4a3a58f6c9ed2ccfc3c3dcab0d16c1c6412f5
Unknown <yusnanda.hj@gmail.com>
2018-11-25 03:57:02 +0100

    Controller, ListsManager

--- src/main/java/server/PeerController.java
+++ src/main/java/server/PeerController.java
@@ -1,25 +1,40 @@
 package server;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import manager.PeersListManager;
+import org.springframework.web.bind.annotation.*;
+import representation.Peer;
+
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 public class PeerController {
 
     @GetMapping("/peers")
-    public void getPeers() {
-        //TODO
+    public List<Peer> getPeersList() {
+        return PeersListManager.readPeers();
     }
 
     @PostMapping("/peers")
-    public void registerPeer() {
-        //TODO
+    public Peer registerPeer() {
+        List<Peer> peersList = PeersListManager.readPeers();
+        Peer registeringPeer = new Peer("toto", "bgTA");
+        if(peersList.stream().noneMatch(o -> o.getPeerId().equals(registeringPeer.getPeerId()))) {
+            peersList.add(registeringPeer);
+        }
+        PeersListManager.savePeers(peersList);
+        return registeringPeer;
     }
 
     @DeleteMapping("/peers/{peerId}")
-    public void unregisterPeer() {
-        //TODO
+    public void unregisterPeer(@PathVariable String peerId) {
+        List<Peer> peersList = PeersListManager.readPeers();
+        for(Iterator<Peer> peerIterator = peersList.iterator(); peerIterator.hasNext();){
+            Peer peer = peerIterator.next();
+            if(peer.getPeerId().equals(peerId)){
+                peerIterator.remove();
+            }
+        }
+        PeersListManager.savePeers(peersList);
     }
 }
